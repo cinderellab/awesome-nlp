16e350784b4579c679ad63fe86882b1a9561cb13
cinderellab <cinderela80@proton.me>
2021-10-08 12:49:19 +0000

    updated descriptions

--- Plex/Regexps.py
+++ Plex/Regexps.py
@@ -62,4 +62,44 @@ def uppercase_range(code1, code2):
 	else:
 		return None
 
-def lowercase_range(c
+def lowercase_range(code1, code2):
+	"""
+	If the range of characters from code1 to code2-1 includes any
+	upper case letters, return the corresponding lower case range.
+	"""
+	code3 = max(code1, ord('A'))
+	code4 = min(code2, ord('Z') + 1)
+	if code3 < code4:
+		d = ord('a') - ord('A')
+		return (code3 + d, code4 + d)
+	else:
+		return None
+
+def CodeRanges(code_list):
+	"""
+	Given a list of codes as returned by chars_to_ranges, return
+	an RE which will match a character in any of the ranges.
+	"""
+	re_list = []
+	for i in xrange(0, len(code_list), 2):
+		re_list.append(CodeRange(code_list[i], code_list[i + 1]))
+	return apply(Alt, tuple(re_list))
+
+def CodeRange(code1, code2):
+	"""
+	CodeRange(code1, code2) is an RE which matches any character
+	with a code |c| in the range |code1| <= |c| < |code2|.
+	"""
+	if code1 <= nl_code < code2:
+		return Alt(RawCodeRange(code1, nl_code), 
+							 RawNewline, 
+							 RawCodeRange(nl_code + 1, code2))
+	else:
+		return RawCodeRange(code1, code2)
+
+#
+#	 Abstract classes
+#
+
+class RE:
+	"""RE is the base class 
