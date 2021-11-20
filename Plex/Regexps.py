32858867e9659f8a5881de70f5925e1ca2911254
cinderellab <cinderela80@proton.me>
2021-11-20 07:24:50 +0000

    fix typo in README.md

--- Plex/Regexps.py
+++ Plex/Regexps.py
@@ -347,4 +347,41 @@ class Alt(RE):
 		for re in re_list:
 			self.check_re(i, re)
 			if re.nullable:
-	
+				nullable_res.append(re)
+				nullable = 1
+			else:
+				non_nullable_res.append(re)
+			if re.match_nl:
+				match_nl = 1
+			i = i + 1
+		self.nullable_res = nullable_res
+		self.non_nullable_res = non_nullable_res
+		self.nullable = nullable
+		self.match_nl = match_nl
+
+	def build_machine(self, m, initial_state, final_state, match_bol, nocase):
+		for re in self.nullable_res:
+			re.build_machine(m, initial_state, final_state, match_bol, nocase)
+		if self.non_nullable_res:
+			if match_bol:
+				initial_state = self.build_opt(m, initial_state, BOL)
+			for re in self.non_nullable_res:
+				re.build_machine(m, initial_state, final_state, 0, nocase)
+
+	def calc_str(self):
+		return "Alt(%s)" % string.join(map(str, self.re_list), ",")
+
+
+class Rep1(RE):
+	"""Rep1(re) is an RE which matches one or more repetitions of |re|."""
+
+	def __init__(self, re):
+		self.check_re(1, re)
+		self.re = re
+		self.nullable = re.nullable
+		self.match_nl = re.match_nl
+
+	def build_machine(self, m, initial_state, final_state, match_bol, nocase):
+		s1 = m.new_state()
+		s2 = m.new_state()
+		initi
