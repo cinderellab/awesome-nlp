fcba69f63d127965a4327d9142faf79b5d9a8275
cinderellab <cinderela80@proton.me>
2022-03-14 21:36:52 +0000

    better documentation

--- containers.py
+++ containers.py
@@ -16,4 +16,39 @@ class sentence:
     words        = None
     spans        = None
     p_tags       = None
-    ta
+    tags         = None
+    sub_links    = None
+    atom         = None
+    pos          = None
+    tag_set      = None
+    constituents = None
+    diagram      = None
+    
+    def __init__(self, sentence, normal_words):
+        self.words      = sentence[0]
+        self.spans      = sentence[1]
+        self.p_tags     = sentence[2]
+        self.tags       = sentence[3]
+        self.sub_links  = sentence[4]
+        self.diagram    = sentence[5].split('\n')
+        self.tag_set    = []
+        
+        self.tagged_words = braubt_tagger.tag(normal_words)
+        self.has_right_wall = ('RIGHT-WALL' in sentence[0] and False or True)
+        self.has_left_wall  = ('LEFT-WALL'  in sentence[0] and False or True)
+        
+
+
+    def __repr__(self):
+        return '<sentence %s>' % (self.words)
+
+
+class iterative_container:
+    def __init__(self):
+        self.hash_set = {}
+        self.current_number = 1
+        
+    def _hash(self, word):
+        for char in word:
+            value = ord(char) << 7
+           
