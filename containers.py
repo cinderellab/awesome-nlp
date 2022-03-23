22d27fd1f165262655b6e463803c15e07a82d421
cinderellab <cinderela80@proton.me>
2022-03-23 05:30:48 +0000

    tweak

--- containers.py
+++ containers.py
@@ -51,4 +51,37 @@ class iterative_container:
     def _hash(self, word):
         for char in word:
             value = ord(char) << 7
-           
+            value = self._c_mul(1000003, value) ^ ord(char)
+            
+        value = value ^ len(word)
+        if value == -1:
+            value = False
+            
+        return value
+    
+    def _c_mul(self, a, b):
+        return eval(hex((long(a) * b) & 0xFFFFFFFFL)[:-1])
+    
+    def add(self, word):
+        if word not in self.hash_set:
+            self.hash_set[word] = self.current_number
+            self.current_number += 1
+            
+        return self.hash_set[word]
+
+class svm_container:
+    words = []
+    tags  = []
+    
+    def __init__(self, container):
+        i = -1
+        for words in container.words:
+            self.words.append({word_container.add(words):container.tags[i]})
+            i += 1
+        #for tags in container.tags:
+        #    self.tags.append(tag_container.add(tags))
+            
+    def __repr__(self):
+        return '<svm tags: %s>' % (self.words)
+        #return '<svm words %s:tags %s>' % (len(self.words), len(self.tags))
+
