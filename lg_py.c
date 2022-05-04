da14a9d5dd1e04262334b03ab84c862e4c20573e
cinderellab <cinderela80@proton.me>
2022-05-04 19:40:50 +0000

    Обновлена база HUD.

--- lg_py.c
+++ lg_py.c
@@ -53,3 +53,44 @@ static PyObject *Sentence_new(PyTypeObject *type, PyObject *args, PyObject *kwds
 
     self = (Sentence *)type->tp_alloc(type, 0);
     if (self != NULL) {
+        self->word_list = PyList_New(0);
+        if (self->word_list == NULL){
+            Py_DECREF(self);
+            return NULL;
+        }
+        
+        self->span_list = PyList_New(0);
+        if (self->span_list == NULL){
+            Py_DECREF(self);
+            return NULL;
+        }
+
+        self->links_list = PyList_New(0);
+        if (self->links_list == NULL){
+            Py_DECREF(self);
+            return NULL;
+        }
+        self->tag_list = PyList_New(0);
+        if (self->tag_list == NULL){
+            Py_DECREF(self);
+            return NULL;
+        }
+        self->tag_pair_list = PyList_New(0);
+        if (self->tag_pair_list == NULL){
+            Py_DECREF(self);
+            return NULL;
+        }
+
+        self->number = 0;
+    }
+
+    return (PyObject *)self;
+}
+
+
+static int Sentence_init(Sentence *self, PyObject *args, PyObject *kwds){
+    PyObject *word_list = NULL;
+    PyObject *span_list = NULL;
+    PyObject *links_list = NULL;
+    PyObject *tag_list = NULL;
+  
