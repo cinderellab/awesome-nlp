d0e33021e8687d17bb3110a739a698d713f32149
cinderellab <cinderela80@proton.me>
2022-05-04 11:25:53 +0000

    remove useless lines

--- lg_py.c
+++ lg_py.c
@@ -19,4 +19,37 @@ static PyTypeObject linkgrammar_SentenceType = {
     0,                         
     "linkgrammar.Sentence",    
     sizeof(Sentence),          
-    0,             
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    0,                         
+    Py_TPFLAGS_DEFAULT,        
+    "linkgrammar objects",     
+};
+static void Sentence_dealloc(Noddy* self){
+    Py_XDECREF(self->word_list);
+    Py_XDECREF(self->span_list);
+    Py_XDECREF(self->links_list);
+    Py_XDECREF(self->tag_list);
+    Py_XDECREF(self->tag_pair_list);
+
+    self->ob_type->tp_free((PyObject*)self);
+}
+
+static PyObject *Sentence_new(PyTypeObject *type, PyObject *args, PyObject *kwds){
+    Sentence *self;
+
+    self = (Sentence *)type->tp_alloc(type, 0);
+    if (self != NULL) {
