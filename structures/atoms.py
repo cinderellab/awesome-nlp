6aecfac7b4a88e9f6d6443d0141e5034a6667e38
cinderellab <cinderela80@proton.me>
2023-09-08 20:35:08 +0000

    PRF&PUB

--- structures/atoms.py
+++ structures/atoms.py
@@ -7,4 +7,54 @@ class Queue:
         self.queue = deque()
 
     def empty(self):
-        if(len(self
+        if(len(self.queue) > 0):
+            return False
+        else:
+            return True
+
+    def count(self):
+        return len(self.queue)
+
+    def add(self, item):
+        self.queue.append(item)	
+
+    def remove(self):
+        item = self.queue[0]
+        self.queue = self.queue[1:]
+        return item
+
+class Stack:
+    def __init__(self):
+        self.s=[]
+
+    def empty(self):
+        if(len(self.s)>0):
+            return 0
+        else:
+            return 1
+
+    def count(self):
+        return len(self.s)
+
+    def push(self, item):
+        ts=[item]
+        for i in self.s:
+            ts.append(i)
+        self.s=ts
+
+    def pop(self):
+        item=self.s[0]
+        self.s=self.s[1:]
+        return item
+
+
+class GraphException(Exception):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return repr(self.value)
+
+class Atoms:
+    ## Atoms are a basic hypergraph
+    def __init__(self):
+        self.next_edge
