67eed982930b3d834b2466520227e88421010415
cinderellab <cinderela80@proton.me>
2023-10-10 16:11:34 +0000

    namespace change

--- structures/fsm.py
+++ structures/fsm.py
@@ -21,4 +21,26 @@ associate:
         (current_state) --> (action, next_state)
 
 You use the add_transition_any() method to add to this transition table. The
-FSM also has one default transition that is not associated wit
+FSM also has one default transition that is not associated with any specific
+input_symbol or state. You use the set_default_transition() method to set the
+default transition.
+
+When an action function is called it is passed a reference to the FSM. The
+action function may then access attributes of the FSM such as input_symbol,
+current_state, or "memory". The "memory" attribute can be any object that you
+want to pass along to the action functions. It is not used by the FSM itself.
+For parsing you would typically pass a list to be used as a stack.
+
+The processing sequence is as follows. The process() method is given an
+input_symbol to process. The FSM will search the table of transitions that
+associate:
+
+        (input_symbol, current_state) --> (action, next_state)
+
+If the pair (input_symbol, current_state) is found then process() will call the
+associated action function and then set the current state to the next_state.
+
+If the FSM cannot find a match for (input_symbol, current_state) it will then
+search the table of transitions that associate:
+
+     
