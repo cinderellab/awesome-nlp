2452e467e736e0787d22a9b47bb7834f7cb25ee4
cinderellab <cinderela80@proton.me>
2023-10-10 06:36:50 +0000

    Fix bower.json

--- structures/fsm.py
+++ structures/fsm.py
@@ -1,4 +1,24 @@
 #!/usr/bin/env python
 
 """This module implements a Finite State Machine (FSM). In addition to state
-this F
+this FSM also maintains a user defined "memory". So this FSM can be used as a
+Push-down Automata (PDA) since a PDA is a FSM + memory.
+
+The following describes how the FSM works, but you will probably also need to
+see the example function to understand how the FSM is used in practice.
+
+You define an FSM by building tables of transitions. For a given input symbol
+the process() method uses these tables to decide what action to call and what
+the next state will be. The FSM has a table of transitions that associate:
+
+        (input_symbol, current_state) --> (action, next_state)
+
+Where "action" is a function you define. The symbols and states can be any
+objects. You use the add_transition() and add_transition_list() methods to add
+to the transition table. The FSM also has a table of transitions that
+associate:
+
+        (current_state) --> (action, next_state)
+
+You use the add_transition_any() method to add to this transition table. The
+FSM also has one default transition that is not associated wit
