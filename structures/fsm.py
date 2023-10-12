91bf0fb0d19b22b699eb0cf353b9c482b7112d63
cinderellab <cinderela80@proton.me>
2023-10-12 12:30:03 +0000

    Release 0.12

--- structures/fsm.py
+++ structures/fsm.py
@@ -122,4 +122,27 @@ class FSM:
 
         The action may be set to None in which case the process() method will
         ignore the action and only set the next_state. The next_state may be
-        set to None in which case the current state will
+        set to None in which case the current state will be unchanged.
+
+        You can also set transitions for a list of symbols by using
+        add_transition_list(). """
+
+        if next_state is None:
+            next_state = state
+        self.state_transitions[(input_symbol, state)] = (action, next_state)
+
+    def add_transition_list(self, list_input_symbols, state, action=None, next_state=None):
+
+        """This adds the same transition for a list of input symbols.
+        You can pass a list or a string. Note that it is handy to use
+        string.digits, string.whitespace, string.letters, etc. to add
+        transitions that match character classes.
+
+        The action may be set to None in which case the process() method will
+        ignore the action and only set the next_state. The next_state may be
+        set to None in which case the current state will be unchanged. """
+
+        if next_state is None:
+            next_state = state
+        for input_symbol in list_input_symbols:
+            self.add_transition (input_symb
