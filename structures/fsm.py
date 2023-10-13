a0ddcff52f3a86b51b1c3366bbf953ffd75d225b
cinderellab <cinderela80@proton.me>
2023-10-13 22:53:17 +0000

    bfd/

--- structures/fsm.py
+++ structures/fsm.py
@@ -145,4 +145,28 @@ class FSM:
         if next_state is None:
             next_state = state
         for input_symbol in list_input_symbols:
-            self.add_transition (input_symb
+            self.add_transition (input_symbol, state, action, next_state)
+
+    def add_transition_any (self, state, action=None, next_state=None):
+
+        """This adds a transition that associates:
+
+                (current_state) --> (action, next_state)
+
+        That is, any input symbol will match the current state.
+        The process() method checks the "any" state associations after it first
+        checks for an exact match of (input_symbol, current_state).
+
+        The action may be set to None in which case the process() method will
+        ignore the action and only set the next_state. The next_state may be
+        set to None in which case the current state will be unchanged. """
+
+        if next_state is None:
+            next_state = state
+        self.state_transitions_any [state] = (action, next_state)
+
+    def set_default_transition (self, action, next_state):
+
+        """This sets the default transition. This defines an action and
+        next_state if the FSM cannot find the input symbol and the current
+        stat
