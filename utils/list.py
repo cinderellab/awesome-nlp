7c7f170e22f8f0ae34cb62b9b7f34276d6747bf3
cinderellab <cinderela80@proton.me>
2023-10-14 18:54:43 +0000

    dmi_scan: fix missing check for _DMI_ signature in smbios_present()

---
+++ utils/list.py
@@ -0,0 +1,6 @@
+from collections import deque
+class list_functions:
+    def flatten(self, List):
+        ### take a list of varied depth
+        ### and flatten it! with no recursion
+        return reduc
